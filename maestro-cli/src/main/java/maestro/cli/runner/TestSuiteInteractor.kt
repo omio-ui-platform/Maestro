a4fd0785762aaa2df08b36b1e371fa92eda990b4
mohammad.haidar <mohammad.haidar@omio.com>
2026-08-06 16:11:00 +0300

    test

--- maestro-cli/src/main/java/maestro/cli/runner/TestSuiteInteractor.kt
+++ maestro-cli/src/main/java/maestro/cli/runner/TestSuiteInteractor.kt
@@ -154,7 +154,6 @@ class TestSuiteInteractor(
                 reportOut,
             )
         }
-        //test the PR checker
         // TODO(bartekpacia): Should it also be saving to debugOutputPath?
         TestDebugReporter.saveSuggestions(aiOutputs, debugOutputPath)
 
